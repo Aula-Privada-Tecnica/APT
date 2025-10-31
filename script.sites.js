b19be9e9e4e33e13db96ee7746266dec290233b7
Aula Privada Tecnica <aulaprivadatecnica@outlook.com>
2025-10-30 22:53:38 -0600

    Update script.sites.js

--- script.sites.js
+++ script.sites.js
@@ -5,24 +5,3 @@ document.addEventListener('DOMContentLoaded', function() {
         window.history.back();
     });
 });
-
-function limpiarBuscador() {
-    const inputBuscador = document.querySelector('.input-busqueda');
-    
-    if (inputBuscador) {
-        inputBuscador.value = "";
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function() {   
-
-    limpiarBuscador(); 
-
-    window.addEventListener('pageshow', function(event) {
-        if (event.persisted) {
-            limpiarBuscador();
-        }
-    });
-
-    configurarModal7Pilares();
-});
